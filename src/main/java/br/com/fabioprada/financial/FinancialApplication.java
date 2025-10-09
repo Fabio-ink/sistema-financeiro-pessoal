b69c19f531fa0e0d1a08947eee3631880b7c8be8
Fabio <fabio.pradaa@gmail.com>
2025-10-09 19:49:49 -0300

    refactor(app): Renomeia aplicação e introduz configuração por ambiente

--- src/main/java/br/com/fabioprada/financial/FinancialApplication.java
+++ src/main/java/br/com/fabioprada/financial/FinancialApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanceiroApplication {
+public class FinancialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinanceiroApplication.class, args);
+		SpringApplication.run(FinancialApplication.class, args);
 	}
 
 }
