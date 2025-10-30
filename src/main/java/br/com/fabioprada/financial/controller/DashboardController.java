2dd55ddcc204350fcf95e46e8fadc336b45ce537
Fabio-ink <fabiopradaa@gmail.com>
2025-10-30 20:27:37 -0300

    feat(dashboard): Add interactive transaction chart

--- src/main/java/br/com/fabioprada/financial/controller/DashboardController.java
+++ src/main/java/br/com/fabioprada/financial/controller/DashboardController.java
@@ -1,6 +1,7 @@
 package br.com.fabioprada.financial.controller;
 
 import br.com.fabioprada.financial.dto.MonthSummaryDTO;
+import br.com.fabioprada.financial.model.Transaction;
 import br.com.fabioprada.financial.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -21,5 +23,10 @@ public class DashboardController {
     @GetMapping("/summary")
     public Map<String, MonthSummaryDTO> getSummary() {
         return dashboardService.getMonthlySummaries();
+    }
+
+    @GetMapping("/transactions")
+    public List<Transaction> getAllTransactions() {
+        return dashboardService.getAllTransactions();
     }
 }
