11fe74f42b6c7d411ab8e4bd757f5cf3c55a4695
Fabio-ink <fabiopradaa@gmail.com>
2025-10-31 21:27:05 -0300

    feat: Implement Monthly Planning backend

---
+++ src/main/java/br/com/fabioprada/financial/model/MonthlyPlanning.java
@@ -0,0 +1,36 @@
+package br.com.fabioprada.financial.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MonthlyPlanning {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int month;
+    private int year;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private BigDecimal estimatedAmount;
+
+    public String getYearMonth() {
+        return String.format("%d-%02d", year, month);
+    }
+
+    public BigDecimal getPlannedValue() {
+        return estimatedAmount;
+    }
+}
