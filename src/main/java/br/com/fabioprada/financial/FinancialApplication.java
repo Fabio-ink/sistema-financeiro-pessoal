ff15c229e4a12faab7dc940be0c69c5867bb3bff
Fabio-ink <fabiopradaa@gmail.com>
2025-11-21 20:47:00 -0300

    refactor: update user authentication and core logic

--- src/main/java/br/com/fabioprada/financial/FinancialApplication.java
+++ src/main/java/br/com/fabioprada/financial/FinancialApplication.java
@@ -3,7 +3,12 @@ package br.com.fabioprada.financial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import br.com.fabioprada.financial.config.JwtProperties;
+import br.com.fabioprada.financial.config.CorsProperties;
+
 @SpringBootApplication
+@EnableConfigurationProperties({ JwtProperties.class, CorsProperties.class })
 public class FinancialApplication {
 
 	public static void main(String[] args) {
